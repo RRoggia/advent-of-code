20b5f54f6ea367bb3e9a9baf8e91e2f82bd070be
RRoggia <renanzr@gmail.com>
2024-12-02 12:20:16 -0300

    compute all possible moves

--- 2016/javascript/day11/day11.js
+++ 2016/javascript/day11/day11.js
@@ -1,143 +1,236 @@
-function Floor(number, items, isElevatorHere) {
-	this.number = number;
-	this.items = items;
+  const initialState = {
+    "HM":1,
+    "LM":1,
+    "HG":2,
+    "LG":3,
+  }
+  const initialState2 = {
+    "POG":1,
+    "POM":2,
+    "TUG":1,
+    "TUM":1,
+    "PRG":1,
+    "PRM":2,
+    "RUG":1,
+    "RUM":1,
+    "COG":1,
+    "COM":1
+  }
+
+  let myFloor = 1
+  const queue = {
+    [createStateKey(initialState, myFloor)]: true
+  }
+  createTree(initialState, myFloor, queue,  0)
+
+function createTree(state, currentFloor, queue, count){
+  if(Object.values(state).filter(f => f===4).length === 4) {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    console.log(count)
+    return
+  }
+  console.log("my state is", createStateKey(state, currentFloor), "and counting", count, ".")
+  let resources = Object.entries(state)
+                    .filter(([resource, floor]) => floor === currentFloor)
+                    .map(([r, f]) => r)
+
+  let validStatesUp = []
+  let validStatesDown = []
+
+  for(let i=0; i < resources.length; i++){
+    const r = resources[i]
+    console.log("processing", r)
+
+    addValidUpperStates(r, state, currentFloor, validStatesUp)
+    addValidLowerStates(r, state, currentFloor, validStatesDown)
+
+    for(let j = i+1; j < resources.length; j++){
+      const r2 = resources[j] 
+      addValidUpperStatesMovingTwo(r, r2, state, currentFloor, validStatesUp)
+      addValidLowerStatesMovingTwo(r, r2, state, currentFloor, validStatesDown)
+      }
+    }
+
+  console.log("---------------") 
+
+  for (let vs of validStatesUp) {
+    createTree(vs, currentFloor + 1, queue, count + 1)
+  }
+
+  for (let vs of validStatesDown) {
+    createTree(vs, currentFloor - 1, queue, count + 1)
+  }
 }
 
-function Building(floors) {
-	this.floors = floors;
-	this.elevatorLocation = 0;
+function getCounterPart(resource){
+  let prefix = resource.substring(0, resource.length - 1)
+  return `${prefix}${isGenerator(resource) ? "M" : "G"}`
 }
 
-function hasFriedChip(items) {
-	var chips = [];
-	var generators = [];
-
-	for (var i = 0; i < items.length; i++) {
-		if (items[i].substring(2,3) === "m"){
-			chips.push(items[i].substring(0,2));
-		}else{
-			generators.push(items[i].substring(0,2));
-		}
-	}
-
-	if (chips.length === 0 || generators.length === 0 )
-		return false;
-
-	if (chips.length > generators.length)
-		return true;
-
-	for (i = 0; i < chips.length; i++) {
-		var isElementFried = true;
-		for (var j = 0; j < generators.length; j++) {
-			if (chips[i] === generators[j]) {
-				isElementFried = false;
-				break;
-			}
-		}
-		if(isElementFried)
-			return isElementFried
-	}
-
-	return false;
+function hasSingleMicroInFloor(state, floor){
+  return Object.entries(state) 
+    .filter(([r,f]) => f === floor)
+    .map(([r,f]) => r)
+    .filter(r => state[getCounterPart(r)] !== floor)
+    .some(r => !isGenerator(r))
 }
 
-function getMovimentsWithoutFriyngAChip(building) {
-	var possibleMovements = [];
-	var items = building.floors[building.elevatorLocation].items;
-
-	for (var i = 0; i < items.length; i++) {
-		var floorItems = items.slice();
-		var item = items[i];
-		floorItems.splice(i, 1);
-
-		if (!hasFriedChip(floorItems)) {
-			if (building.elevatorLocation < 3) {
-				var upFloorItems = building.floors[building.elevatorLocation + 1].items.slice();
-				upFloorItems.push(item);
-				if (!hasFriedChip(upFloorItems)) {
-					possibleMovements.push({
-						moviment: "up",
-						items: [item]
-					});
-				}
-			}
-
-			if (building.elevatorLocation > 0) {
-				var downFloorItems = building.floors[building.elevatorLocation - 1].items.slice();
-				downFloorItems.push(item);
-				if (!hasFriedChip(downFloorItems)) {
-					possibleMovements.push({
-						moviment: "down",
-						items: [item]
-					});
-				}
-			}
-		}
-	}
-
-	for (var i = 0; i < items.length; i++) {
-		for (var j = i + 1; j < items.length; j++) {
-			var floorItems = items.slice();
-			
-			var item1 = items[i];
-			var item2 = items[j];
-			
-			floorItems.splice(i, 1);
-			floorItems.splice(j - 1, 1);
-
-			if (!hasFriedChip(floorItems)) {
-
-				if (building.elevatorLocation < 3) {
-					var upFloorItems = building.floors[building.elevatorLocation + 1].items.slice();
-					upFloorItems.push(item1);
-					upFloorItems.push(item2);
-					if (!hasFriedChip(upFloorItems)) {
-						possibleMovements.push({
-							moviment: "up",
-							items: [item1, item2]
-						});
-					}
-				}
-
-				if (building.elevatorLocation > 0) {
-					var downFloorItems = building.floors[building.elevatorLocation - 1].items.slice();
-					downFloorItems.push(item1);
-					downFloorItems.push(item2);
-					if (!hasFriedChip(downFloorItems)) {
-						possibleMovements.push({
-							moviment: "down",
-							items: [item1, item2]
-						});
-					}
-				}
-			}
-		}
-	}
-	
-	return possibleMovements;
+function isGenerator(resource){
+  return resource[resource.length -1] === "G"
 }
 
-var floors = [
-	new Floor(1, ["pog", "thg", "thm", "prg", "rug" , "rum", "cog", "com"]),
-	new Floor(2, ["pom", "prm"]),
-	new Floor(3, []),
-	new Floor(4, []),
-];
+function canMove(state, resource ) {
+  const sameFloor = state[getCounterPart(resource)] === state[resource]
+  
+  if(sameFloor) {
+    (isGenerator(resource) && !Object.entries(state)
+      .some(([r,f]) => r !== resource && f === state[resource] && isGenerator(r)))
+  } 
 
-var building = new Building(floors);
-var step = 0;
-//while(building.floors[3].items.length < 10){
+  isGenerator(resource)
+}
 
-	//check if has any fried chip
-	for (var i = 0; i < building.floors.length; i++) {
-		hasFriedChip(building.floors[i].items);
-	}
+function isValidState(state) {
+  //console.log("validate state", state)
+ return isValidFloor(Object.entries(state)
+    .filter(([r,f]) => f === 1)
+    .map(([r,f]) => r)) &&
+  isValidFloor(Object.entries(state)
+    .filter(([r,f]) => f === 2)
+    .map(([r,f]) => r)) &&
+  isValidFloor(Object.entries(state)
+    .filter(([r,f]) => f === 3)
+    .map(([r,f]) => r)) &&
+  isValidFloor(Object.entries(state)
+    .filter(([r,f]) => f === 4)
+    .map(([r,f]) => r))
+}
 
-	var elevatorMovimentsWithoutFriyngAChip = getMovimentsWithoutFriyngAChip(building);
+function isValidFloor(floorResources){
+  //console.log(floorResources)
+  const pool = {}
+  
+  for(let i =0; i < floorResources.length; i++){
+    const resource = floorResources[i]
+    const key = isGenerator(resource) ? getCounterPart(resource) : resource
+    //console.log("key", key)
+    if(key in pool){
+      delete pool[key]
+      //console.log("found, delte it", key)
+    }else{
+      pool[key] = resource
+      //console.log("missing, add", key)
+    }
+ } 
+  //console.log(pool)
+  let hasG = false
+  let hasM = false
+  
+  for(let v of Object.values(pool)) {
+    if(isGenerator(v)){
+       hasG = true
+    } else {
+      hasM = true
+    }
+  }
+  //console.log("isvalid floor", !(hasG && hasM))
+  return !(hasG && hasM)
+}
 
 
-	step++;
-//}
+function createStateKey(state, floor) {
+  return floor.toString() + JSON.stringify(state)
+}
 
+function addValidUpperStates(r, state, currentFloor, validStates) {
+  if( currentFloor + 1 < 5) {
+    console.log("might have upper floor")
+    const newState = {...state}
+    newState[r] = currentFloor + 1 
+    const newStateKey = createStateKey(newState, currentFloor + 1)
+    console.log(newStateKey)
+    console.log("not in queue", !queue[newStateKey])
+    console.log("isValid", isValidState(newState))
+    if(!queue[newStateKey] && isValidState(newState)) {
+        queue[newStateKey] = true
+        console.log("add new possible state", newStateKey, " moving up", r)
+        validStates.push(newState)
+    }
+  }
+}
 
+function addValidLowerStates(r, state, currentFloor, validStates) {
+  if(currentFloor - 1 > 0) {
+    console.log("might have lower floor")
+    const newState = {...state}
+    newState[r] = currentFloor - 1 
+    const newStateKey = createStateKey(newState, currentFloor - 1)
+    console.log(newStateKey)
+    console.log("not in queue", !queue[newStateKey])
+    console.log("isValid", isValidState(newState))
+    if(!queue[newStateKey] && isValidState(newState)) {
+      queue[newStateKey] = true
+      console.log("add new possible state", newStateKey, " moving down", r)
+      validStates.push(newState)
+    }
+  }
+}
 
+function addValidUpperStatesMovingTwo(r, r2, state, currentFloor, validStates) {
+  if( currentFloor + 1 < 5) {
+    const newState = {...state}
+    newState[r] = currentFloor + 1 
+    newState[r2] = currentFloor + 1
+    const newStateKey = createStateKey(newState, currentFloor + 1)
+    console.log(newStateKey)
+    console.log("not in queue", !queue[newStateKey])
+    console.log("isValid", isValidState(newState))
+    if(!queue[newStateKey] && isValidState(newState)) {
+      queue[newStateKey] = true
+      console.log("add new possible state", newStateKey, " moving up", r, r2)
+      validStates.push(newState)
+    }
+  }
+}
+
+function addValidLowerStatesMovingTwo(r, r2, state, currentFloor, validStates) {
+  if(currentFloor - 1 > 0) {
+    const newState = {...state}
+    newState[r] = currentFloor - 1 
+    newState[r2] = currentFloor - 1 
+    const newStateKey = createStateKey(newState, currentFloor - 1)
+    console.log(newStateKey)
+    console.log("not in queue", !queue[newStateKey])
+    console.log("isValid", isValidState(newState))
+    if(!queue[newStateKey] && isValidState(newState)) {
+      queue[newStateKey] = true
+      console.log("add new possible state", newStateKey, " moving down", r, r2)
+      validStates.push(newState)
+    }
+  }
+}
+
+// generator
+// if alone
+// counterpart || no single micro
+// if together
+// duo is micro || no micro
+//
+// micro
+// if alone
+// counterpart || no generator
+// if together
+// duo is generator || no generator
+//
+//
+// generator
+//
+// can I move?
+// if alone
+//  counterpart || no single micro in nextfloor 
+// if together
+// no single micro in nextfloor && no generator in currentfloor
+//
+//
+// all floors
+// dont have generators pairing with micro
